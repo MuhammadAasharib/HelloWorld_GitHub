80b4ade3a48f0c7ae1f96441dffe41db5530c8fe
MuhammadAasharib <aasharib@icloud.com>
2016-09-07 01:51:51 +0500

    second commit

--- HelloWorld_GitHub/Source.cpp
+++ HelloWorld_GitHub/Source.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 	cout << "Hello GitHub World" << endl;
+	
+
+	string name;
+
+	cout << "What is your name??"<< endl;
+	getline(cin,name);
 	system("pause");
+
 	return 0;
 }
